0223645c39b8c42e0ec395db9d287cd709579250
Daniel Laible <daniellaible@gmail.com>
2026-04-24 17:05:20 +0200

    implemented basic select statement

--- src/command/select.rs
+++ src/command/select.rs
@@ -1,53 +1,39 @@
 use crate::command::whereclause::WhereClause;
+
 use regex::Regex;
 use crate::command::sqloperator::Operator;
 use crate::table::datatype::DataType;
 
+#[derive(Debug)]
 pub struct Select {
     table_name: String,
     columns: Vec<String>,
     where_clause: WhereClause,
 }
 
-pub fn parse_stmt(stmt: String) -> Select {
-    let select: Select = Select::default();
-
-    let columns: Vec<&str> = get_columns(&stmt);
-    for mut column in columns {
-        column = column.trim();
-        println!("{}", column);
-    }
-
-    let tablename = match get_table_name(&stmt) {
-        Some(x) => x,
-        None => "doof",
-    };
-
+pub fn parse_stmt(stmt: String) -> Select{
+    let mut select: Select = Select::default();
+    select.columns = get_columns(&stmt);
+    select.table_name = get_table_name(&stmt).unwrap_or_else(|| "doof").parse().unwrap();
     if check_for_where(&stmt) {
-        let clause: WhereClause = get_where_clause(&stmt);
-        println!("{:?}", clause);
+        select.where_clause = WhereClause::parse(&stmt);
+        println!("{:?}", select);
+        select
+    }else{
+        println!("{:?}", select);
+        select
     }
-    println!("{}", tablename);
-    select
 }
 
-fn get_where_clause(stmt: &String) -> WhereClause {
-    let regex = Regex::new(r"(?i)\bwhere\b\s+([\s\S]*)").unwrap();
-    let captures = regex.captures(stmt).unwrap();
-    let where_as_string = captures.get(1).unwrap().as_str();
-
-    let x: Vec<&str> = where_as_string.split("=").collect();
-    let where_clause = WhereClause::new(String::from(x[0]), Operator::EQUAL, DataType::Int {x: 1});
-    println!("{}", where_as_string);
-    where_clause
-}
-
-fn get_columns(stmt: &String) -> Vec<&str> {
+fn get_columns(stmt: &String) -> Vec<String> {
     let regex = Regex::new(r"(?i)\bselect\b\s*([\s\S]*?)\s+\bfrom\b\s+").unwrap();
     let captures = regex.captures(stmt).unwrap();
     let columns_as_string = captures.get(1).unwrap().as_str();
- 
-    let parts: Vec<&str> = columns_as_string.split(",").collect();
+    let single_column:Vec<&str> = columns_as_string.split(",").collect();
+    let mut parts: Vec<String> = Vec::new();
+    for single_column in single_column {
+        parts.push(single_column.to_string());
+    }
     parts
 }
 
@@ -61,8 +47,9 @@ fn check_for_where(stmt: &String) -> bool {
     if reg.is_match(stmt){
         return true;
     }
-    return false;
+    false
 }
+
 
 impl Select {
     pub fn default() -> Self {
@@ -76,23 +63,59 @@ impl Select {
 
 #[cfg(test)]
 mod tests {
-    use crate::command::select::parse_stmt;
+    use crate::command::select::{parse_stmt, Select};
+    use crate::command::sqloperator::Operator;
+    use crate::command::whereclause::WhereClause;
+
+
 
     #[test]
     fn simple_select_with_where_clause() {
         let select = "SELECT name, country FROM population WHERE id=1";
-        parse_stmt(String::from(select));
+        let select: Select = parse_stmt(String::from(select));
+        let clause: WhereClause = select.where_clause;
+        assert_eq!(select.table_name, "population");
+        assert_eq!(select.columns.len(), 2);
+        assert_eq!(clause.get_operator(), Operator::EQUAL);
     }
 
     #[test]
     fn simple_select_with_where_clause_lowercase() {
         let select = "select name, country from population where id=1";
-        parse_stmt(String::from(select));
+        let select: Select = parse_stmt(String::from(select));
+        let clause: WhereClause = select.where_clause;
+        assert_eq!(select.table_name, "population");
+        assert_eq!(select.columns.len(), 2);
+        assert_eq!(clause.get_operator(), Operator::EQUAL);
+    }
+
+    #[test]
+    fn simple_select_with_where_clause_less_than() {
+        let select = "select name, country from population where id<100";
+        let select: Select = parse_stmt(String::from(select));
+        let clause: WhereClause = select.where_clause;
+        assert_eq!(select.table_name, "population");
+        assert_eq!(select.columns.len(), 2);
+        assert_eq!(clause.get_operator(), Operator::LESSER);
+    }
+
+    #[test]
+    fn simple_select_with_where_clause_greater_than() {
+        let select = "select name, country from population where id>100";
+        let select: Select = parse_stmt(String::from(select));
+        let clause: WhereClause = select.where_clause;
+        assert_eq!(select.table_name, "population");
+        assert_eq!(select.columns.len(), 2);
+        assert_eq!(clause.get_operator(), Operator::GREATER);
     }
 
     #[test]
     fn simple_select_without_where_clause() {
         let select = "SELECT name, country FROM population";
-        parse_stmt(String::from(select));
+        let select: Select = parse_stmt(String::from(select));
+        let clause: WhereClause = select.where_clause;
+        assert_eq!(select.table_name, "population");
+        assert_eq!(select.columns.len(), 2);
+        assert_eq!(clause.get_operator(), Operator::UNDEFINED);
     }
 }
--- src/command/whereclause.rs
+++ src/command/whereclause.rs
@@ -1,5 +1,6 @@
 use crate::command::sqloperator::Operator;
 use crate::table::datatype::DataType;
+use regex::Regex;
 
 #[derive(Clone, Debug)]
 pub struct WhereClause {
@@ -9,15 +10,52 @@ pub struct WhereClause {
 }
 
 impl WhereClause {
-    pub fn new(column: String, operator: Operator, value: DataType ) -> Self {
-        WhereClause{column, operator, value}
+    pub fn new(column: String, operator: Operator, value: DataType) -> Self {
+        WhereClause { column, operator, value }
     }
-    
-    pub fn default() -> Self{
-        WhereClause{
+
+    pub fn get_operator(&self) -> Operator {
+        self.operator.clone()
+    }
+
+    pub fn default() -> Self {
+        WhereClause {
             column: String::default(),
             operator: Operator::UNDEFINED,
             value: DataType::Undefined,
         }
+    }
+
+    pub fn parse(stmt: &String) -> WhereClause {
+        let regex = Regex::new(r"(?i)\bwhere\b\s+([\s\S]*)").unwrap();
+        let captures = regex.captures(stmt).unwrap();
+        let where_as_string = captures.get(1).unwrap().as_str();
+
+        let mut splits: Vec<&str> = vec!();
+        let mut operator: Operator = Operator::UNDEFINED;
+        if where_as_string.contains("=") {
+            splits = where_as_string.split("=").collect();
+            operator = Operator::EQUAL;
+        } else if where_as_string.contains("<") {
+            splits = where_as_string.split("<").collect();
+            operator = Operator::LESSER;
+        } else if where_as_string.contains(">") {
+            splits = where_as_string.split(">").collect();
+            operator = Operator::GREATER;
+        } else if where_as_string.contains("<=") {
+            splits = where_as_string.split("<=").collect();
+            operator = Operator::LESSEROREQ;
+        } else if where_as_string.contains(">=") {
+            splits = where_as_string.split(">=").collect();
+            operator = Operator::GREATEROREQ;
+        } else if where_as_string.contains("!=") {
+            splits = where_as_string.split("!=").collect();
+            operator = Operator::NOTEQUAL;
+        }
+
+        splits[0] = splits[0].trim();
+        println!("{:?}", splits);
+        let where_clause = WhereClause::new(String::from(splits[0]), operator, DataType::Int { x: splits[1].parse().unwrap() });
+        where_clause
     }
 }
