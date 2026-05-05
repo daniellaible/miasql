55ec313a3471dc43496163534ce91afd8b2e99de
Daniel Laible <daniellaible@gmail.com>
2026-05-05 19:46:03 +0200

    wip insert command - finished retrieving tablename and columns

--- src/command/insert.rs
+++ src/command/insert.rs
@@ -1,7 +1,6 @@
 use crate::command::whereclause::WhereClause;
-use regex::Regex;
-use crate::command::select::Select;
 use crate::table::datatype::DataType;
+use regex::Regex;
 
 #[derive(Debug)]
 pub struct Insert {
@@ -13,16 +12,44 @@ pub struct Insert {
 
 pub fn parse_stmt(stmt: String) -> Insert {
     let mut insert: Insert = Insert::default();
-    let table:String = get_table(&stmt);
+    let table: String = get_table(&stmt);
+    let columns: Vec<String> = get_columns(&stmt);
     insert
 }
 
+fn get_columns(stmt: &String) -> Vec<String> {
+    let table_n_columns: &str = retrieve_table_n_columns(&stmt);
+    let re = Regex::new(r"\((.*)\)").unwrap();
+    let mut temp_columns = Vec::new();
+    if let Some(caps) = re.captures(table_n_columns) {
+        let inside = caps.get(1).unwrap().as_str();
+        temp_columns = inside.split(',').collect::<Vec<&str>>();
+    }
+
+    let mut columns: Vec<String> = Vec::new();
+    for  column in temp_columns.iter() {
+        let c = column.trim();
+        columns.push(c.to_string());
+    }
+    columns
+}
+
 fn get_table(stmt: &String) -> String {
-    let regex = Regex::new(r"(?i)\binsert\b\s*into\b\s*([\s\S]*?)\s+\bvalues\b\s+").unwrap();
-    let captures = regex.captures(stmt).unwrap();
-    let what_we_got = captures.get(1).unwrap().as_str();
-    println!("{:?}", &what_we_got);
-    String::new()
+    let table_n_columns: &str = retrieve_table_n_columns(&stmt);
+
+    //This regex gets you all until the first opening brace
+    let regex_table = Regex::new(r"^\s*([^(]*?)\s*(?:\(|$)").unwrap();
+    let captures_table = regex_table.captures(table_n_columns).unwrap();
+    let table_name = captures_table.get(1).unwrap().as_str();
+    String::from(table_name)
+}
+
+fn retrieve_table_n_columns(stmt: &String) -> &str{
+    let regex_table_n_columns =
+        Regex::new(r"(?i)\binsert\b\s*into\b\s*([\s\S]*?)\s+\bvalues\b\s+").unwrap();
+    let captures_table_n_columns = regex_table_n_columns.captures(stmt).unwrap();
+    let table_n_columns = captures_table_n_columns.get(1).unwrap().as_str();
+    table_n_columns
 }
 
 impl Insert {
@@ -42,7 +69,8 @@ mod tests {
 
     #[test]
     fn simple_select_with_where_clause() {
-        let select = "INSERT INTO user (first_name, last_name, age) VALUES ('daniel', 'mayer', '35') ";
+        let select =
+            "INSERT INTO user (first_name, last_name, age) VALUES ('daniel', 'mayer', '35') ";
         parse_stmt(String::from(select));
     }
 }
